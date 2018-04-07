8236853819031203819ac438b3a1550b20e85792
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-04-07 14:47:59 +0900

    refactored version 1.1

--- app.js
+++ app.js
@@ -1,136 +1,217 @@
+/////////////////////////////////////////////////
+////// Initial Variables
+
+//home url
 let url =
     "https://newsapi.org/v2/top-headlines?country=us&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
 
+// DOM selectors
 const home = document.querySelector("#home");
 const business = document.querySelector("#business");
 const technology = document.querySelector("#technology");
 const sports = document.querySelector("#sports");
 const science = document.querySelector("#science");
 const entertainment = document.querySelector("#entertainment");
+const menu = document.getElementsByClassName("navLink");
 const listSpace = document.querySelector("#listSpace");
 
-const fetchArticles = () => {
+/////////////////////////////////////////////////
+////// Initial News Page Function
+fetchArticles();
+
+/////////////////////////////////////////////////
+////// Fetch News Function
+
+// fetch articles function
+function fetchArticles() {
     fetch(url)
         .then(r => {
             return r.json();
         })
         .then(data => {
             let articles = data.articles;
-            let listSpace = document.querySelector("#listSpace");
-            listSpace.innerHTML = "";
-            let articleList = document.createElement("ul");
-            articleList.classList = "ui stackable divided items";
-            listSpace.appendChild(articleList);
+            createList();
             articles.map(story => {
-                let individualStory = document.createElement("li");
-                individualStory.className = "item";
-                let imageDiv = document.createElement("div");
-                imageDiv.className = "ui small bordered fluid image";
-                let imageSource = document.createElement("img");
-                let imageURL = "";
-                if (story.urlToImage === null) {
-                    imageURL = "no-image-box.png";
-                } else {
-                    imageURL = story.urlToImage;
-                }
-                imageSource.setAttribute("src", imageURL);
-                let contentDiv = document.createElement("div");
-                contentDiv.className = "content";
-                let headerDiv = document.createElement("div");
-                headerDiv.className = "header";
-                let mainTitle = document.createElement("a");
-                let linkSetting = story.url;
-                mainTitle.setAttribute("href", linkSetting);
-                mainTitle.innerText = story.title;
-
-                let metaDiv = document.createElement("div");
-                metaDiv.className = "meta";
-                let nameSpan = document.createElement("div");
-                nameSpan.className = "name";
-                nameSpan.innerText = story.author;
-                let dateSpan = document.createElement("div");
-                dateSpan.className = "date";
-                dateSpan.innerText = story.publishedAt;
-                let descriptionDiv = document.createElement("div");
-                descriptionDiv.className = "description";
-                let storyDescription = document.createElement("p");
-                storyDescription.innerText = story.description;
-                let breaker = document.createElement("br");
-
-                articleList.appendChild(individualStory);
-                individualStory.appendChild(imageDiv);
-                imageDiv.appendChild(imageSource);
-                individualStory.appendChild(contentDiv);
-                contentDiv.appendChild(headerDiv);
-                headerDiv.appendChild(mainTitle);
-                contentDiv.appendChild(metaDiv);
-                metaDiv.appendChild(nameSpan);
-                metaDiv.appendChild(breaker);
-                metaDiv.appendChild(dateSpan);
-                contentDiv.appendChild(descriptionDiv);
-                descriptionDiv.appendChild(storyDescription);
+                createStory();
+                createImage(story);
+                createContentHeader();
+                createTitle(story);
+                createMeta();
+                createName(story);
+                createDate(story);
+                createDescription(story);
+                htmlConstruction();
             });
         })
         .catch(e => {
             console.log(`An error occurred: ${e}`);
         });
-};
+}
 
-const resetActive = () => {
-    home.classList = "yellow item";
-    business.classList = "yellow item";
-    technology.classList = "yellow item";
-    sports.classList = "yellow item";
-    science.classList = "yellow item";
-    entertainment.classList = "yellow item";
-};
+/////////////////////////////////////////////////
+////// Create List Functions
 
-fetchArticles();
+// empty list item variables for scope
+let articleList = (individualStory = imageDiv = imageSource = contentDiv = headerDiv = mainTitle = metaDiv = nameSpan = dateSpan = descriptionDiv = storyDescription = breaker = null);
+
+//create list
+function createList() {
+    // reset list area
+    listSpace.innerHTML = "";
+    //create list and append to list area
+    articleList = document.createElement("ul");
+    articleList.classList = "ui stackable divided items";
+    listSpace.appendChild(articleList);
+}
+
+//create individual story list item
+function createStory() {
+    individualStory = document.createElement("li");
+    individualStory.className = "item";
+}
+
+// create story image
+function createImage(story) {
+    imageDiv = document.createElement("div");
+    imageDiv.className = "ui small bordered fluid image";
+    imageSource = document.createElement("img");
+    // get list url or add no image image to source
+    let imageURL = "";
+    if (story.urlToImage === null) {
+        imageURL = "no-image-box.png";
+    } else {
+        imageURL = story.urlToImage;
+    }
+    imageSource.setAttribute("src", imageURL);
+}
+
+// create content and header div
+function createContentHeader() {
+    //  creates content div
+    contentDiv = document.createElement("div");
+    contentDiv.className = "content";
+    // creates header div
+    headerDiv = document.createElement("div");
+    headerDiv.className = "header";
+}
+
+// create main title and link to story
+function createTitle(story) {
+    mainTitle = document.createElement("a");
+    let linkSetting = story.url;
+    mainTitle.setAttribute("href", linkSetting);
+    mainTitle.innerText = story.title;
+}
+
+// create meta div for item
+function createMeta() {
+    metaDiv = document.createElement("div");
+    metaDiv.className = "meta";
+}
+
+// create author
+function createName(story) {
+    // create story author
+    nameSpan = document.createElement("div");
+    nameSpan.className = "name";
+    nameSpan.innerText = story.author;
+}
+
+// create date and shorten
+function createDate(story) {
+    // create story date
+    function storyDate(story) {
+        let storyDate = story.publishedAt;
+        return storyDate.toString().substring(0, 10);
+    }
+    dateSpan = document.createElement("div");
+    dateSpan.className = "date";
+    dateSpan.innerText = storyDate(story);
+}
+
+// create story description
+function createDescription(story) {
+    // create description div
+    descriptionDiv = document.createElement("div");
+    descriptionDiv.className = "description";
+    // create description paragraph
+    storyDescription = document.createElement("p");
+    storyDescription.innerText = story.description;
+    breaker = document.createElement("br");
+}
+
+// append all list item elements to the list
+function htmlConstruction() {
+    articleList.appendChild(individualStory);
+    individualStory.appendChild(imageDiv);
+    imageDiv.appendChild(imageSource);
+    individualStory.appendChild(contentDiv);
+    contentDiv.appendChild(headerDiv);
+    headerDiv.appendChild(mainTitle);
+    contentDiv.appendChild(metaDiv);
+    metaDiv.appendChild(nameSpan);
+    metaDiv.appendChild(breaker);
+    metaDiv.appendChild(dateSpan);
+    contentDiv.appendChild(descriptionDiv);
+    descriptionDiv.appendChild(storyDescription);
+}
+
+/////////////////////////////////////////////////
+////// Button Functions
+
+// adds active class to menu items
+function changeActive(id) {
+    for (let i = 0; i < menu.length; i++) {
+        menu[i].classList = "yellow item navLink";
+    }
+    id.classList = "yellow active item navLink";
+}
+
+// create url string for next category
+function createUrl(navId) {
+    let newsTopic = navId.innerText.toLowerCase();
+    if (newsTopic === "home") {
+        url =
+            "https://newsapi.org/v2/top-headlines?country=us&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
+    } else {
+        url =
+            "https://newsapi.org/v2/top-headlines?country=us&category=" +
+            newsTopic +
+            "&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
+    }
+    return url;
+}
+
+// function for grouping button events together
+function navButtonClick(buttonID) {
+    changeActive(buttonID);
+    createUrl(buttonID);
+    fetchArticles();
+}
+
+/////////////////////////////////////////////////
+////// Button Event Listeners
 
 home.addEventListener("click", () => {
-    url =
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
-    fetchArticles();
-    resetActive();
-    home.classList = "yellow active item";
+    navButtonClick(home);
 });
 
 business.addEventListener("click", () => {
-    url =
-        "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
-    fetchArticles();
-    resetActive();
-    business.classList = "yellow active item";
+    navButtonClick(business);
 });
 
 technology.addEventListener("click", () => {
-    url =
-        "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
-    fetchArticles();
-    resetActive();
-    technology.classList = "yellow active item";
+    navButtonClick(technology);
 });
 
 sports.addEventListener("click", () => {
-    url =
-        "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
-    fetchArticles();
-    resetActive();
-    sports.classList = "yellow active item";
+    navButtonClick(sports);
 });
 
 science.addEventListener("click", () => {
-    url =
-        "https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
-    fetchArticles();
-    resetActive();
-    science.classList = "yellow active item";
+    navButtonClick(science);
 });
 
 entertainment.addEventListener("click", () => {
-    url =
-        "https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=bb3e2cffda7d4f0c8f9b8c46c8bbd8ca";
-    fetchArticles();
-    resetActive();
-    entertainment.classList = "yellow active item";
+    navButtonClick(entertainment);
 });
--- readme.md
+++ readme.md
@@ -2,11 +2,24 @@
 
 ## Assignment
 
-The purpose of this assignment was to use an API for the first time and display it on a page. I used the `fetch` method to make AJAX requests and built the site using HTML, CSS, and Vanilla JavaScript. It was my first time using an API in conjunction with HTML. I play on refactoring some of the code in the future, along with a few design tweeks along the way.
+The purpose of this assignment was to use an API for the first time and display it on a page. I used the `fetch` method to make AJAX requests and built the site using HTML, CSS, and Vanilla JavaScript. I used [News API](https://newsapi.org) to get headlines from major news categories and the page can be updated and viewed easily. It was my first time using an API in conjunction with HTML. I plan on refactoring some of the code in the future, adding a search function, and making a few design tweeks along the way.
 
-## Technolgies Used
+## Technologies Used
 
 *   HTML
 *   CSS
-*   JavaScript
+*   JavaScript (Vanilla)
 *   Web API
+
+## Changelog
+
+*   [1.0] 2017-04-05
+
+-   Basic page built.
+-   Built up Javascript function for fetch and click functions.
+
+*   [1.1] 2017-04-07
+
+-   Refactored functions for fetch and click.
+-   Added Comments
+-   Changed menu styling.
